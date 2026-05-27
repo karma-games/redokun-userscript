badd8094de6b9c458e9122a85edcc5ad4753de9f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2026-05-27 19:45:27 +0200

    update to current redokun UI

--- redokun.user.js
+++ redokun.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://raw.githubusercontent.com/karma-games/redokun-violentmonkey-userscript/refs/heads/main/redokun.user.js
 // @grant       GM_addStyle
 // @run-at      document-start
-// @version     1.0
+// @version     1.1
 // @author      Stefan Seifert
 // ==/UserScript==
 
@@ -15,12 +15,12 @@
 
   GM_addStyle(`
 
-/* this should apply only to translation editor with previs image */
+/* this should apply only to translation editor with preview image */
 @media only screen and (min-width: 1500px) and (min-height: 1024px) {
-  .mx-auto.w-full.max-w-6xl:has(img[src*="/thumbs/"]) {
+  .mx-auto.w-full.max-w-8xl:has(img[src*="/thumbs/"]) {
     max-width: 100%;
   }
-  .sticky.mr-8.w-80.bg-gray-100:has(img[src*="/thumbs/"]) {
+  .sticky.mr-8.w-80:has(img[src*="/thumbs/"]) {
     width: 40rem;
   }
 }
